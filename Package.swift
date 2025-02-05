539796ec599350f48c3037fc59bbe95e497df1b0
GitHub Action <action@github.com>
2025-02-05 10:39:12 +0000

    chore: Update to GoogleCast SDK version 4.8.3

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleCast",
-            url: "https://github.com/dushantSW/googlecast-ios-sdk-xcframeworks/releases/download/VERSION/GoogleCast.xcframework.zip",
-            checksum: "CHECKSUM"
+            url: "https://github.com/dushantSW/googlecast-ios-sdk-xcframeworks/releases/download/4.8.3/GoogleCast.xcframework.zip",
+            checksum: "be52337c798b014214e53c4e536c6f8b8b41316492dc44edb6ddf168e7bc9a62"
         )
     ]
 )
